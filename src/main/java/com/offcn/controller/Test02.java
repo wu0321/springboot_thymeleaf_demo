b53d62bcea7c21f716dd21377b80eae42612c50d
liuhuiqun <liuhuiqun@offcn.com>
2021-04-19 15:23:00 +0800

    分支上创建测试方法

---
+++ src/main/java/com/offcn/controller/Test02.java
@@ -0,0 +1,15 @@
+package com.offcn.controller;
+
+/**
+ * @Auther: lhq
+ * @Date: 2021/4/19 15:22
+ * @Description:
+ */
+public class Test02 {
+
+    public void test(){
+        while(true){
+
+        }
+    }
+}
