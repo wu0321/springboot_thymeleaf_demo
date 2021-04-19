cf1449eb32e20d481f01de2edb176f15297bbc0a
liuhuiqun <liuhuiqun@offcn.com>
2021-04-19 15:05:25 +0800

    编写了一个测试方法

---
+++ src/main/java/com/offcn/controller/TestController01.java
@@ -0,0 +1,17 @@
+package com.offcn.controller;
+
+/**
+ * @Auther: lhq
+ * @Date: 2021/4/19 14:59
+ * @Description:
+ */
+public class TestController01 {
+
+
+    public void test(){
+        boolean flag = true;
+        if(flag){
+            System.out.println("----");
+        }
+    }
+}
